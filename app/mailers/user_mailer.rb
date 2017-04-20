5e2ed4fe8e3f1595ea36ecc47082f827c9da78b3
stephlynpancho <stephlynpancho@gmail.com>
2017-04-20 12:38:13 -0700

    editappmailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,4 +1,15 @@
 class UserMailer < ApplicationMailer
     default from: "stephlynpancho@gmail.com"
 
+		def welcome_email(email, contact_name, event, user, longitude, latitude)
+		@email = email
+		@contact_name = contact_name
+		@event = event
+		@user = user
+		@longitude = longitude
+		@latitude = latitude
+		mail(to: @email, subject: "This is a message from No Worries")
+	end 
+
 	
+end
